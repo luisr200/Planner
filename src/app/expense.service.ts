3151e42c8339520a55224640a6568f7f4645c020
Luisr200 <luisr200@msn.com>
2019-02-05 17:33:24 -0600

    http client 1

--- src/app/expense.service.ts
+++ src/app/expense.service.ts
@@ -1,22 +1,54 @@
 import { Injectable } from '@angular/core';
-import {EXPENSES} from './mock-expenses'
-import {Expense} from './expense'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Expense } from './expense'
+import { EXPENSES } from './mock-expenses'
+import { catchError, map, tap } from 'rxjs/operators';
 import { Observable, of, empty } from 'rxjs';
-import {MessageService} from './message.service';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseService {
 
-  constructor(private messageService: MessageService) { }
+  private expensesUrl = 'api/expenses';  // URL to web api
+
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
   getExpenses(): Observable<Expense[]> {
-    return of(EXPENSES);
+    return this.http.get<Expense[]>(this.expensesUrl)
+    .pipe(
+      catchError(this.handleError('getHeroes', []))
+    );
   }
 
   getExpense(id: number): Observable<Expense> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(EXPENSES.find(exp => exp.id === id))
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,47 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Expense } from './expense';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const expenses = [
+      {
+          id: 1,
+          amount: 15000,
+          title: 'Rent',
+          type: 'monthly'
+        },
+        {
+          id: 2,
+          amount: 1500,
+          title: 'Gasolina',
+          type: 'monthly'
+        },
+        {
+          id: 3,
+          amount: 2000,
+          title: 'Renta',
+          type: 'monthly'
+        },
+        {
+          id: 4,
+          amount: 500000,
+          title: 'Comida',
+          type: 'monthly'
+        }
+      ];
+    return {expenses};
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(expenses: Expense[]): number {
+    return expenses.length > 0 ? Math.max(...expenses.map(expense => expense.id)) + 1 : 11;
+  }
+}
